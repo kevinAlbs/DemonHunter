ee62ad215206303778bb0d72e5b57d12065c5e8d
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2016-11-20 00:03:25 -0500

    Fix file submission

--- submit.php
+++ submit.php
@@ -1,8 +1,6 @@
 <?php
 require_once('mysqlconfig.php');
 if (isset($_GET['n']) && isset($_GET['s'])) {
-    $file = fopen('scores.txt', 'a');
-    if ($file === FALSE) die('file error');
     $name = base64_decode($_GET['n']);
     if ($name === FALSE) die('name error');
     $score = base64_decode($_GET['s']);
